321467380fe4b2c24d65f071768232f69e733b49
jeancharlesx64 <jean.albuquerque@sptech.school>
2025-03-01 17:44:13 -0300

    [FEAT] :: creating a basic components using ReactJS and styled-components

---
+++ src/components/Example/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExampleWrapper from "./style";
+
+const Example = ()=>{
+    return(
+        <ExampleWrapper>
+            <h1>This is a "Example" component</h1>
+        </ExampleWrapper>
+    );
+}
+
+export default Example;
---
+++ src/components/Example/style.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components"; 
+
+const ExampleWrapper = styled.div`
+    height: 200px;
+    width: 200px;
+    background-color: red;
+    border: 5px solid black;
+`
+
+export default ExampleWrapper;
